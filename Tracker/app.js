d8cf757a280c88c210bb6639ff968fe26c7016c2
sankhayan1919 <sankhayansadhukhan71@gmail.com>
2025-02-25 23:46:06 +0530

    First

---
+++ Tracker/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'Tracker', 'public')));
+app.set('views', path.join(__dirname, 'Tracker', 'views'));
+app.set('view engine', 'ejs');
+io.on("connection", function(socket){
+    socket.on("send-location", function(data){
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id);
+    })
+});
+app.get('/', function (req, res){
+    res.render("index");
+});
+
+server.listen(3000);
--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const db = require('./Bagshop/config/mongoose-connection');
-// const userModel = require('./Bagshop/models/user');
-// const postModel = require('./Bagshop/models/post');
-const cookieParser = require('cookie-parser');
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-// const crypto = require('crypto');
-// const multer = require('multer');
-// const multerconfig = require('./Bagshop/multerconfig');
-const ownersRouter = require('./Bagshop/routes/ownersRouter');
-const usersRouter = require('./Bagshop/routes/usersRouter');
-const productsRouter = require('./Bagshop/routes/productsRouter');
+const app = express();
+const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static(path.join(__dirname, 'Bagshop', 'public')));
-app.set('views', path.join(__dirname, 'Bagshop', 'views'));
+app.use(express.static(path.join(__dirname, 'Notetaker', 'public')));
+app.set('views', path.join(__dirname, 'NoteTaker', 'views'));
 app.set('view engine', 'ejs');
-app.use(cookieParser());
 
-app.use('/owners', ownersRouter);
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
+app.get('/', function(req, res){
+  fs.readdir(`./NoteTaker/files`, function(err, files){
+    res.render("index", {files: files});
+  });
+});
+app.get('/file/:filename', function(req, res){
+  fs.readFile(`./NoteTaker/files/${req.params.filename}`, "utf-8", function(err, filedata){
+    res.render('show', {filename: req.params.filename, filedata: filedata}); 
+  });
+});
+app.get('/edit/:filename', function(req, res){
+  res.render('edit', {filename: req.params.filename, filedata: req.params.filedata});
+});
+app.post('/edit', function(req, res){
+  fs.rename(`./NoteTaker/files/${req.body.previous}`, `./NoteTaker/files/${req.body.current}`, function(err){
+    res.redirect("/");
+  })
+});
+app.post('/create', function(req, res){
+  fs.writeFile(`./NoteTaker/files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+    res.redirect("/")
+  });
+});
 
-app.listen(3000);
+app.listen(3001);
